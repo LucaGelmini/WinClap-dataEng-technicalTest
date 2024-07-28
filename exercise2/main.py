d771959fa74e6f72ebd24bb15ad0a6daebd76636
Luca <lucagelmini@hotmail.com>
2024-07-28 18:08:57 -0300

    exercise 2 corrected. OK!

--- exercise2/main.py
+++ exercise2/main.py
@@ -3,32 +3,35 @@ from os import path
 NUMBERS_FILE = path.join(path.dirname(__file__), "c-input.in")
 
 
-def sheep_sleeper(T: int):
+def sheep_sleeper() -> list[str]:
+
+    with open(NUMBERS_FILE) as file:
+        T = int(file.readline())
+        N_LIST = [int(line) for line in file.readlines()]
+
     if T < 1 or T > 100:
-        raise ValueError("T must be >= 1 and <= 100")
-    numbers = [int(w.strip()) for w in open(NUMBERS_FILE)]
-    check_digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-    results = []
+        raise ValueError(f"Infringed T constrains: 0 <= T <= 200; N = {T}")
 
-    def check_for_one_test(test_number: int):
-        seen_numbers = set()
-        # If N can be equal to 0 as the exercise ask (0 ≤ N ≤ 200), the the examples given were wrong.
-        # For example the 11 case, as it starts from 0, then just need to take N to 9 to reach all digits.
-        # So the answer in 11 case must be 99 instead of 110.
-        # Im using 1 <= N <= 200
-        for N in range(1, 201):
-            product = test_number * N
-            for digit in set(str(product)):
-                seen_numbers.add(int(digit))
-            if check_digits.issubset(seen_numbers):
-                return product
-        return "INSOMNIA"
+    check_digits = set("0123456789")
 
-    # checking for all tests numbers
-    for test_number in numbers[:T]:
-        results.append(check_for_one_test(test_number))
-    return results
+    reults: list[str] = []
+    for i in range(T):
+        N = N_LIST[i]
+        if N < 0 or N > 200:
+            raise ValueError(f"Infringed T constrains: 1 <= T <= 100; T = {T}")
+        if N == 0:
+            reults.append("INSOMNIA")
+        else:
+            seen_digits = set()
+            multiplier = 1
+            while seen_digits != check_digits:
+                product = N * multiplier
+                seen_digits.update(str(product))
+                multiplier += 1
+            reults.append(str(product))
+    return reults
 
 
 if __name__ == "__main__":
-    print(sheep_sleeper(100))
+    for case, output in enumerate(sheep_sleeper()):
+        print(f"Case #{case+1}: {output}")
